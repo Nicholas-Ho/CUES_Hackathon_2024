eaafa048505a254f450e4537467ff7c65de95e48
bzdxgg <bzdxgg@gmail.com>
2024-11-02 17:34:31 +0000

    yes

--- frontend_logic/cat.py
+++ frontend_logic/cat.py
@@ -1,7 +1,7 @@
 def create_fat_sad_cat(fatness=2):
     spaces = ' ' * fatness
     fatness_sad = f"""
-{spaces}/\\_/\\  
+{spaces}/\_/\  
 ({spaces}T_T{spaces}) 
 >{spaces} ^{spaces} <  
 """
@@ -10,7 +10,7 @@ def create_fat_sad_cat(fatness=2):
 def create_fat_happy_cat(fatness=2):
     spaces = ' ' * fatness
     fatness_happy = f"""
-{spaces}/\\_/\\  
+{spaces}/\_/\  
 ({spaces}o.o{spaces}) 
 >{spaces} ^{spaces} <  
 """
@@ -19,7 +19,7 @@ def create_fat_happy_cat(fatness=2):
 
 def dead_cat(number=0):
     cat_lines = [
-    "   /\\_/\\  " * number,
+    "   /\_/\  " * number,
     "  ( x_x ) " * number,
     "   > ^ <  " * number
     ]
@@ -29,29 +29,29 @@ def dead_cat(number=0):
 def diabetes():
     cat_with_diabetes = r"""
                   ________________
-                 /I have diabetes,\\
+                 /I have diabetes,\
                  |please reduce   |
                  |sugar.          |
-                  \\_______________/
+                  \_______________/
     """
     return cat_with_diabetes
 
 def high_blood_pressure():
     cat = r"""
                   _________________
-                 / I have high blood\\
+                 / I have high blood\
                  |pressure, please   |
                  |reduce salt intake.|
-                  \\_________________/
+                  \_________________/
     """
     return cat
 
 def high_cholestrol():
     cat = r"""
                   _________________
-                 / I have high      \\
+                 / I have high      \
                  |cholestrol, please |
                  |reduce fat intake. |
-                  \\_________________/
+                  \_________________/
     """
     return cat
